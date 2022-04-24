cba4a7860cd354fb4f40735d9a050c32577e03a7
mbagas <karab4946@gmail.com>
2022-04-24 17:04:54 +0700

    fix: add relation to database and role seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $roles = [
+            [
+                'role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'role' => 'kasir',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+        Role::insert($roles);
+    }
+}
